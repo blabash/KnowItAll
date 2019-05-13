261a26153c9b1415f2f0fa0fedbf6a75d3ce36da
brett <brett.labash@gmail.com>
2019-05-12 17:58:43 -0700

    Remove tweet related stuff

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -19,7 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
