f2bf047952d8ab3c1a1753f8ac76a2c71a79f2ca
brett <brett.labash@gmail.com>
2019-05-12 17:17:58 -0700

    Add user validations for login and register routes

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,17 +4,28 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the users route"})
 });
 
-router.post('/register', (req, res) => {
-   //  const { errors, isValid } = validateRegisterInput(req.body);
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+   res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email
+   });
+})
 
-   //  if (!isValid) {
-   //      return res.status(400).json(errors);
-   //  }
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
 
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -36,7 +47,11 @@ router.post('/register', (req, res) => {
                             .then(user => {
                               const payload = { id: user.id, name: user.name };
 
-                              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                              jwt.sign(
+                                 payload, 
+                                 keys.secretOrKey, 
+                                 { expiresIn: 3600 }, 
+                                 (err, token) => {
                                  res.json({
                                     success: true,
                                     token: "Bearer " + token
@@ -51,11 +66,11 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-   // const { errors, isValid } = validateLoginInput(req.body);
+   const { errors, isValid } = validateLoginInput(req.body);
 
-   // if(!isValid) {
-   //    return res.status(400).json(errors);
-   // }
+   if(!isValid) {
+      return res.status(400).json(errors);
+   }
 
     const email = req.body.email;
     const password = req.body.password;
